ee29ac971624b0aab64e876938cffbb6426e2e83
Vinnie Dantas <dantasvinniecode@gmail.com>
2019-06-23 20:12:54 -0300

    add tether and change bootstrap import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//
 //= require jquery
-//= require bootstrap/dist/js/bootstrap
+//= require tether/dist/js/tether
+//= require bootstrap/dist/js/bootstrap.bundle
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
